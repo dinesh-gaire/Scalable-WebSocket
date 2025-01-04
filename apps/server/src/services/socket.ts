ca80798370397fde7a54441771df8c9fa1c8bf6a
dinesh-gaire <gairedinesh132@gmail.com>
2025-01-04 18:01:48 +0545

    store message to db

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,24 +1,25 @@
 import Redis from 'ioredis';
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
+import prismaClient from './prisma';
 
 const pub = new Redis({
     host: "localhost",
     port: 6379,
     username: "default",
     password: "",
-  });
-  
-  const sub = new Redis({
+});
+
+const sub = new Redis({
     host: "localhost",
     port: 6379,
     username: "default",
     password: "",
-  });
+});
 
 class SocketService {
     private _io: Server;
 
-    constructor(){
+    constructor() {
         console.log("Init Socket Service...");
         this._io = new Server({
             cors: {
@@ -29,29 +30,35 @@ class SocketService {
         sub.subscribe("MESSAGES");
     }
 
-    public initListeners(){
+    public initListeners() {
         const io = this.io;
         console.log('Init Socket Listeners...')
-        
+
         io.on("connect", (socket) => {
             console.log(`New Socket Connected`, socket.id);
             socket.on("event:message", async ({ message }: { message: string }) => {
-              console.log("New Message Rec.", message);
-              // publish this message to redis
-              await pub.publish("MESSAGES", JSON.stringify({ message }));
+                console.log("New Message Rec.", message);
+                // publish this message to redis
+                await pub.publish("MESSAGES", JSON.stringify({ message }));
             });
         });
-          
+
         sub.on("message", async (channel, message) => {
             if (channel === "MESSAGES") {
-              console.log("new message from redis", message);
-              io.emit("message", message);
+                console.log("new message from redis", message);
+                io.emit("message", message);
+                //DBStore
+                await prismaClient.message.create({
+                    data: {
+                        text: message,
+                    }
+                })
             }
         });
-          
+
     }
 
-    get io(){
+    get io() {
         return this._io;
     }
 }
